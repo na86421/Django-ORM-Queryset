f8552543146136d7a0134c23c3b679e1317e2440
yjk <na86421@naver.com>
2021-04-18 23:13:11 +0900

    prefetch_related(), select_related()

--- mysite2/models.py
+++ mysite2/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-
+ 
+class Blog(models.Model):
+    name = models.CharField(max_length=50)
+ 
+ 
+class Category(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+ 
+ 
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
