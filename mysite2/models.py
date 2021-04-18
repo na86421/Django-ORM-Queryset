0b67c1a351c213206f87f747778f9995d3fdf6f0
yjk <na86421@naver.com>
2021-04-18 23:04:17 +0900

    ORM과 Queryset의 특징

---
+++ mysite2/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
